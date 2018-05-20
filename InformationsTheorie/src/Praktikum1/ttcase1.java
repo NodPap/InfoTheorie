2a15db7b170ff4f98c25bf02f713a5ca03a07d84
nod <nod@nod>
2018-05-20 18:48:28 +0200

    t

---
+++ InformationsTheorie/src/Praktikum1/ttcase1.java
@@ -0,0 +1,20 @@
+package Praktikum1;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class ttcase1 {
+
+	boolean t = true;
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+    void pruf()
+    {
+    	assertTrue(t);
+    	
+    }
+
+}
