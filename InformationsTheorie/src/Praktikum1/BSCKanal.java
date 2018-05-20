f751d613854a5fe0d114c43cccd2e5d93bf44c6b
root <root@kali>
2018-05-20 20:39:15 +0200

    praktikum1 aufgabe 1-2

---
+++ InformationsTheorie/src/Praktikum1/BSCKanal.java
@@ -0,0 +1,12 @@
+package Praktikum1;
+
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+public class BSCKanal {
+	
+	
+	public static void main(String[] args) {
+		
+	}
+}
--- InformationsTheorie/src/Praktikum1/funktions.java
+++ InformationsTheorie/src/Praktikum1/funktions.java
@@ -1,11 +1,7 @@
 package Praktikum1;
 
-import java.io.BufferedWriter;
-import java.io.DataOutputStream;
-import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
-import java.io.ObjectOutputStream;
 import java.io.OutputStreamWriter;
 
 public class funktions {
@@ -27,7 +23,18 @@ public class funktions {
 		return logfakulat(n) - logfakulat(n-k) - logfakulat(k);
 	}
 	
-	public static void writeFile1(funktions f,int n, int k) throws IOException {
+	public static double log2(double x) {
+		if(x == 0) {
+			return 1;
+		}
+		return Math.log(x)/Math.log(2);
+	}
+	
+	public static double bsc_kanal_fehler(double p) {
+		return 1 + p * log2(p) + (1-p) * log2(1-p);
+	}
+	
+	public static void writeFile_binomPlug(funktions f,int n, int k) throws IOException {
 		try {
 
 			OutputStreamWriter writer = new OutputStreamWriter(
@@ -46,12 +53,31 @@ public class funktions {
 			e.printStackTrace();
 		}
 	}
+	public static void writeFile_bscPlug(double p) throws IOException {
+		try {
+
+			OutputStreamWriter writer = new OutputStreamWriter(
+                    new FileOutputStream("bscPlug.dat"), "UTF-8");
+			writer.write("# X Y \n");
+			writer.flush();
+			while (p>=0)
+			{
+				 writer.write(p+" "+bsc_kanal_fehler(p)+"\n");
+				p-=0.111;
+				writer.flush();
+			}
+			writer.close();
+			
+		}catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
 	public static void main(String[] args) throws IOException {
 		funktions f = new funktions();
 		int n = 1000;
 		int k = 1000;
-		f.writeFile1(f,n,k);
-		
+		//f.writeFile_binomPlug(f,n,k);
+		writeFile_bscPlug(1.0);
 	}
 }
 	
